d5b817642d1152a78bfcf0b786776ab6aa43530e
Wendy Chun <wendy.hj.chun@gmail.com>
2018-03-27 21:16:32 -0400

    Add Footer to the App.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Footer from './components/Footer'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+	  HJELLO
+		<Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+	render() {
+		return (
+			<div class="fa-3x">
+				<a href="https://www.linkedin.com/in/wendyhchun" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+				<a href="https://github.com/whchun" target="_blank"><i class="fab fa-github"></i></a>
+			</div>
+		);
+	}
+}
+
+export default Footer;

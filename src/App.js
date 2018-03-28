4e6681676fc3f274af786b1c240b636d7017e727
Wendy Chun <wendy.hj.chun@gmail.com>
2018-03-27 22:31:38 -0400

    Update footer and main.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Header from './components/Header'
+import Main from './components/Main'
 import Footer from './components/Footer'
 import logo from './logo.svg';
 import './App.css';
@@ -7,7 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-	  HJELLO
+		<Header />
+		<Main />
+		<p />
 		<Footer />
       </div>
     );
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+	render() {
+		return (
+			<div class="main">
+				<h1>Wendy H. Chun</h1>
+				<h3>Software Engineer</h3>
+			</div>
+		);
+	}
+}
+
+export default Main;
